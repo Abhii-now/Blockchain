7d8eb615e99d33df056a65a174c5d1073749fb19
Abhinav <jainabhinav2k16@gmail.com>
2020-05-23 02:33:45 +0530

    new updates

--- blockchain.py
+++ blockchain.py
@@ -5,13 +5,12 @@ import json
 
 # block must consist of data of transaction, hash of previous block,
 class block:
-    def getHash(self, data):
-        return
-    def __init__(self, index, transaction, prev_hash):
+    def __init__(self, index, transaction, previous_hash):
         self.index = index
         self.transaction = transaction
-        self.hash = prev_hash
-    def compute_hash(self, block):
+        self.previous_hash = previous_hash
+
+    def compute_hash(self):
         """
         Returns the hash of the block instance by first converting it
         into JSON string.
@@ -20,14 +19,82 @@ class block:
         return sha256(block_string.encode()).hexdigest()
 
 
+# consisting of the trivial operations performed by a blockchain
 class blockchain:
-    def initTransaction(self, amount):
-        pass
+    difficulty = 2
 
     def __init__(self):
+        self.unconfirmed_transactions = []
         self.chain = []
+        self.create_genesis_block()
 
     def create_genesis_block(self):
         genesis_block = block(0, [], "0")
+        genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
 
+    @property
+    def last_block(self):
+        """
+        A quick pythonic way to retrieve the most recent block in the chain. Note that
+        the chain will always consist of at least one block (i.e., genesis block)
+        """
+        return self.chain[-1]
+
+    def proof_of_work(self, block):
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith("0" * blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    @classmethod
+    def is_valid_proof(cls, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (
+            block_hash.startswith("0" * blockchain.difficulty)
+            and block_hash == block.compute_hash()
+        )
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        """
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out proof of work.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = block(
+            index=last_block.index + 1,
+            transaction=self.unconfirmed_transactions,
+            previous_hash=last_block.hash,
+        )
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
